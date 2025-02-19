16a11c26b3ed1e473105bb2412feb74c77d43b88
CCD311834 <Colinianatkari@gmail.com>
2025-02-19 16:20:14 +0100

    H6 gedaan

--- app/Http/Controllers/meldingenController.php
+++ app/Http/Controllers/meldingenController.php
@@ -2,16 +2,56 @@
 
 //Variabelen vullen
 $attractie = $_POST['attractie'];
+$type = $_POST['type'];
 $capaciteit = $_POST['capaciteit'];
+if(isset($_POST['prioriteit']))
+{$prioriteit = 1;}
+else
+{$prioriteit = 0;}
 $melder = $_POST['melder'];
-
-echo $attractie . " / " . $capaciteit . " / " . $melder;
+$overig = $_POST['overig'];
 
 //1. Verbinding
 require_once '../../../config/conn.php';
 
-//2. Query
+//2. Query (om alles in te vullen wat niet NULL kan zijn)
+$query = "INSERT INTO meldingen (attractie, type, capaciteit, prioriteit, melder, overige_info)
+VALUES(:attractie, :type, :capaciteit, :prioriteit, :melder, :overige_info)";
 
 //3. Prepare
+$statement = $conn->prepare($query);
 
 //4. Execute
+$statement->execute([
+    ":attractie" => $attractie,
+    ":type" => $type,
+    ":capaciteit" => $capaciteit,
+    ":prioriteit" => $prioriteit,
+    ":melder" => $melder,
+    ":overige_info" => $overig
+]);
+
+//Errorinoes
+if(empty($attractie))
+{
+    $errors[] = "Vul de attractie-naam in.";
+}
+if(empty($type))
+{
+    $errors[] = "Kies de type.";
+}
+if(!is_numeric($capaciteit)) 
+{
+    $errors[] = "Vul voor capaciteit een geldig getal in.";
+}
+if(empty($melder))
+{
+    $errors[] = "Vul de melder's naam in.";
+}
+if(isset($errors))
+{
+ var_dump($errors);
+ die();
+}
+
+header("Location: ../../../resources/views/meldingen/index.php?msg=Melding opgeslagen");
--- resources/views/meldingen/create.php
+++ resources/views/meldingen/create.php
@@ -10,7 +10,7 @@
 <body>
 
     <?php require_once __DIR__.'/../components/header.php'; ?>
-
+    
     <div class="container">
         <h1>Nieuwe melding</h1>
 
@@ -21,16 +21,34 @@
                 <input type="text" name="attractie" id="attractie" class="form-input">
             </div>
             <div class="form-group">
-                <label for="type">Type</label>
-                <!-- hier komt een dropdown -->
+                <label for="type">Type:</label>
+                <select name="type" id="type" class="form-input" required>
+                    <option value="">– kies een type –</option>
+                    <option value="achtbaan">Achtbaan</option>
+                    <option value="waterattractie">Waterattractie</option>
+                    <option value="draaimolen">Draaimolen</option>
+                    <option value="kinderattractie">Kinderattractie</option>
+                    <option value="horeca">Horeca</option>
+                    <option value="show">Show</option>
+                    <option value="overig">Overig</option>
+                </select>
             </div>
+
             <div class="form-group">
                 <label for="capaciteit">Capaciteit p/uur:</label>
                 <input type="number" min="0" name="capaciteit" id="capaciteit" class="form-input">
             </div>
             <div class="form-group">
+                <label for="prioriteit">Prioriteit:</label>
+                <input type="checkbox" name="prioriteit" id="prioriteit">
+            </div>
+            <div class="form-group">
                 <label for="melder">Naam melder:</label>
                 <input type="text" name="melder" id="melder" class="form-input">
+            </div>
+            <div class="form-group">
+                <label for="overig">Overige info:</label>
+                <textarea name="overig" id="overig" class="form-input" rows="4"></textarea>
             </div>
 
             <input type="submit" value="Verstuur melding">
