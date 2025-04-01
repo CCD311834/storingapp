947b32a8db60ea864ae719d29d6e7d69ae3a4c6e
CCD311834 <Colinianatkari@gmail.com>
2025-04-01 16:39:46 +0200

    je kunt nu uitloggen

--- login.php
+++ login.php
@@ -1,14 +1,19 @@
-    
-    <div class="container home">
-        <form action="app/Http/Controllers/loginController.php" method="POST">
-            <div class="form-group">
-                <label for="username">Gebruikersnaam:</label>
-                <input type="text" name="username" id="username">
-            </div>
-            <div class="form-group">
-                <label for="password">Wachtwoord:</label>
-                <input type="password" name="password" id="password">
-            </div>
-            <input type="submit" value="Login">
-        </form>
-    </div>
+<div class="container home">
+    <?php
+    if(isset($_GET['msg']))
+    {
+        echo "<div class='msg'>" . $_GET['msg'] . "</div>";
+    }
+    ?>
+    <form action="app/Http/Controllers/loginController.php" method="POST">
+        <div class="form-group">
+            <label for="username">Gebruikersnaam:</label>
+            <input type="text" name="username" id="username">
+        </div>
+        <div class="form-group">
+            <label for="password">Wachtwoord:</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <input type="submit" value="Login">
+    </form>
+</div>
